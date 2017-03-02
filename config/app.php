07f3c7bb5be1f8c4c49530c09ef3727d57449d6b
Paolo Dávila <pdavila@iesebre.com>
2017-03-02 19:33:41 +0100

    Inicializate llum boot

--- config/app.php
+++ config/app.php
@@ -171,6 +171,10 @@ return [
         Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         #llum_providers
 
         /*
