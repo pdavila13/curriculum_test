27a72e8c9a48cf001d27b049b48a8a1742598239
Paolo Dávila <pdavila@iesebre.com>
2017-03-23 21:51:31 +0100

    Added seed: AdminUserSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AdminUserSeeder::class);
         $this->call(CurriculumSeeder::class);
     }
 }
