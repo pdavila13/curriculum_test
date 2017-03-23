1f5137fa971a87c264319417f7b9dbf2f1809d6d
Paolo Dávila <pdavila@iesebre.com>
2017-03-23 19:46:34 +0100

    Use Scool\Curriculum\Models\Study

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Scool\Curriculum\Study;
+use Scool\Curriculum\Models\Study;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +18,11 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    //    Route::get('/link1', function ()    {
-//        // Uses Auth Middleware
-//    });
+    Route::get('/profile/tokqens', function () {
+        return view('tokens');
+    });
 
+    //Route::resource('studies', 'StudiesController');
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
 });
