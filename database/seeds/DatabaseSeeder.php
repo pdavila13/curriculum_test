c7377604439975096f6d78ebb0080b4e24540c95
Paolo Dávila <pdavila@iesebre.com>
2017-03-30 21:16:57 +0200

    Added StudiesSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(AdminUserSeeder::class);
         $this->call(CurriculumSeeder::class);
+        $this->call(StudiesSeeder::class);
     }
 }
